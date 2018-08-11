937ce2292e12c89818314b1d77b45f6bc4ca094e
Faheem Pervez <trippin1@gmail.com>
2018-08-11 22:11:42 +0100

    Add code

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# lock_helper
-Does things on KDE lock/unlock
+# Lock Helper
+This does the following when locking your screen in KDE (and maybe with other DEs' screen saver solutions too):
+
+* Disables the Ctrl+Alt+Bksp X server killing sequence if enabled
+* Disables the Magic SysRq key if enabled
+* Locks VT switching; Ctrl+Alt+F1 etc. will have no effect
+* Runs `kcm_init touchpad` upon unlock to workaround an annoying KDE bug as of lately in that your touchpad settings like Natural Scrolling are discarded
+
+Naturally, this is all reversed on unlock.
+
+# Problems
+
+* `lock_helper` runs as setuid root to lock VT switching and disable the sysrq key. **There could very well be security issues lurking in this code**
+* The X server layout code...
+    * It's really only been designed for a one-keyboard system. Maybe it won't mess up anything if more than one keyboard is present. I don't know.
+    * Other layout options are only retrieved on the start of the program. If you make changes, like swapping Ctrl + Caps Lock using the keyboard tweaks section of your DE, during the runtime of this program then this program will probably revert them on unlock.
+    * No attempt is made to parse funny options strings. `lock_helper` assumes a plain, comma-seperated list with no spaces.
+
+# Installation
+
+```
+cc -Wall -O2 `pkg-config --cflags --libs xkbfile x11 gio-unix-2.0` -DXKB_BASE=\"$(pkg-config --variable xkb_base xkeyboard-config)\" lock_helper.c -o lock_helper
+sudo install --group=root --mode=4755 --owner=root --strip ./lock_helper /usr/local/sbin/
+```
+
+Add `/usr/local/sbin/lock_helper` to your DE's autostart mechanism.
---
+++ lock_helper.c
@@ -0,0 +1,301 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/ioctl.h>
+#include <linux/vt.h>
+#include <fcntl.h>
+
+#include <X11/Xlib.h>
+#include <X11/XKBlib.h>
+#include <X11/extensions/XKBrules.h>
+
+#include <glib.h>
+#include <glib-unix.h>
+#include <gio/gio.h>
+
+#define SYSRQ_PATH "/proc/sys/kernel/sysrq"
+#define MAGIC_TERMINATE_OPTION "terminate:ctrl_alt_bksp"
+
+static uid_t orig_user = -1;
+static GMainLoop *loop = NULL;
+static GDBusProxy *screensaver_proxy = NULL;
+static int term = -1;
+static char orig_sysrq[4];
+static gboolean modify_sysrq;
+static gboolean modify_x11_layout_options;
+static gchar *extra_x11_layout_options = NULL;
+
+static void cleanup()
+{
+    if (term != -1) {
+        close(term);
+        term = -1;
+    }
+    setuid(orig_user);
+    seteuid(orig_user);
+
+    g_clear_pointer(&screensaver_proxy, g_object_unref);
+    g_clear_pointer(&loop, g_main_loop_unref);
+    g_clear_pointer(&extra_x11_layout_options, g_free);
+}
+
+static gboolean on_sigint(gpointer data_ptr G_GNUC_UNUSED)
+{
+    g_main_loop_quit(loop);
+    return G_SOURCE_REMOVE;
+}
+
+static gboolean read_sysrq()
+{
+    int fd = open(SYSRQ_PATH, O_RDONLY);
+    if (fd == -1) {
+        perror("Failed to open sysrq for reading");
+        return FALSE;
+    }
+
+    ssize_t nread = read(fd, orig_sysrq, sizeof(orig_sysrq));
+    if (nread == -1) {
+        perror("Failed to read sysrq");
+        return FALSE;
+    }
+    close(fd);
+
+    orig_sysrq[nread] = '\0';
+
+    return TRUE;
+}
+
+static void write_sysrq(const char *val)
+{
+    int fd = open(SYSRQ_PATH, O_WRONLY);
+    if (fd == -1) {
+        perror("Failed to open sysrq for writing");
+        close(fd);
+        return;
+    }
+
+    if (write(fd, val, strlen(val)) == -1)
+        perror("Failed to write sysrq");
+
+    close(fd);
+}
+
+static void lockVT(gboolean lock)
+{
+    // Thanks to sflock
+    if (term == -1) {
+        if ((term = open("/dev/console", O_RDWR)) == -1) {
+            perror("error opening console");
+            return;
+        }
+    }
+
+    if (ioctl(term, lock ? VT_LOCKSWITCH : VT_UNLOCKSWITCH) == -1)
+        perror("VT_(UN)LOCKSWITCH");
+}
+
+static gboolean must_we_mess_with_x11s_layout(gchar **extra_options)
+{
+    gboolean has_terminate_ctrl_alt_bksp = FALSE;
+    int major = XkbMajorVersion, minor = XkbMinorVersion;
+
+    Display *dpy = XkbOpenDisplay(NULL, NULL, NULL, &major, &minor, NULL);
+    if (dpy) {
+        XkbRF_VarDefsRec vd;
+
+        if (XkbRF_GetNamesProp(dpy, NULL, &vd)) {
+            if (vd.options) {
+                if (!strcmp(vd.options, MAGIC_TERMINATE_OPTION))
+                    has_terminate_ctrl_alt_bksp = TRUE;
+                else {
+                    const size_t mto_len = sizeof(MAGIC_TERMINATE_OPTION) - 1;
+
+                    char *mto_pos;
+                    while ((mto_pos = strstr(vd.options, MAGIC_TERMINATE_OPTION))) {
+                        size_t mto_len2 = strlen(mto_pos);
+                        has_terminate_ctrl_alt_bksp = TRUE;
+
+                        if (mto_len2 > mto_len) {
+                            //memset(mto_pos, '\0', mto_len);
+                            memmove(mto_pos, mto_pos + mto_len + (mto_pos[mto_len] == ','), mto_len2 - mto_len);
+                        } else {
+                            if (*(mto_pos - 1) == ',')
+                                *(mto_pos - 1) = '\0';
+                        }
+                    }
+
+                    *extra_options = g_strdup(vd.options);
+                }
+                free(vd.options);
+            }
+
+            if (vd.model)
+                free(vd.model);
+            if (vd.layout)
+                free(vd.layout);
+            if (vd.variant)
+                free(vd.variant);
+        }
+        XCloseDisplay(dpy);
+    }
+
+    return has_terminate_ctrl_alt_bksp;
+}
+
+static void mess_with_x11s_layout(gboolean add)
+{
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        if (setuid(orig_user) != -1) {
+            // Taken from the Mutter source code
+            int major = XkbMajorVersion, minor = XkbMinorVersion;
+
+            Display *dpy = XkbOpenDisplay(NULL, NULL, NULL, &major, &minor, NULL);
+            if (dpy) {
+                XkbRF_VarDefsRec xkb_var_defs;
+                char *rules = NULL;
+
+                if (XkbRF_GetNamesProp(dpy, &rules, &xkb_var_defs) && rules) {
+                    gchar *rules_file_path = rules[0] != '/' ? g_build_filename(XKB_BASE, "rules", rules, NULL) : g_strdup(rules);
+                    g_clear_pointer(&rules, free);
+
+                    if (xkb_var_defs.options)
+                        free(xkb_var_defs.options);
+
+                    if (add)
+                        xkb_var_defs.options = extra_x11_layout_options ? g_strconcat(MAGIC_TERMINATE_OPTION, ",", extra_x11_layout_options, NULL) : g_strdup(MAGIC_TERMINATE_OPTION);
+                    else
+                        xkb_var_defs.options = g_strdup(extra_x11_layout_options);
+
+                    XkbRF_RulesRec *xkb_rules = XkbRF_Load(rules_file_path, NULL, True, True);
+                    if (xkb_rules) {
+                        XkbComponentNamesRec xkb_comp_names = { 0 };
+                        XkbRF_GetComponents(xkb_rules, &xkb_var_defs, &xkb_comp_names);
+
+                        XkbDescRec *xkb_desc = XkbGetKeyboardByName(dpy,
+                                                                    XkbUseCoreKbd,
+                                                                    &xkb_comp_names,
+                                                                    XkbGBN_AllComponentsMask,
+                                                                    XkbGBN_AllComponentsMask &
+                                                                    (~XkbGBN_GeometryMask), True);
+
+                        if (xkb_desc) {
+                            XkbFreeKeyboard(xkb_desc, 0, True);
+                            gchar *rules_name = g_path_get_basename(rules_file_path);
+                            XkbRF_SetNamesProp(dpy, rules_name, &xkb_var_defs);
+                            g_free(rules_name);
+                        }
+
+                        if (xkb_comp_names.keymap)
+                            free(xkb_comp_names.keymap);
+                        if (xkb_comp_names.keycodes)
+                            free(xkb_comp_names.keycodes);
+                        if (xkb_comp_names.types)
+                            free(xkb_comp_names.types);
+                        if (xkb_comp_names.compat)
+                            free(xkb_comp_names.compat);
+                        if (xkb_comp_names.symbols)
+                            free(xkb_comp_names.symbols);
+                        if (xkb_comp_names.geometry)
+                            free(xkb_comp_names.geometry);
+
+                        XkbRF_Free(xkb_rules, True);
+                    }
+
+                    g_free(xkb_var_defs.options);
+                    g_free(rules_file_path);
+                    if (xkb_var_defs.model)
+                        free(xkb_var_defs.model);
+                    if (xkb_var_defs.layout)
+                        free(xkb_var_defs.layout);
+                    if (xkb_var_defs.variant)
+                        free(xkb_var_defs.variant);
+                }
+
+                XCloseDisplay(dpy);
+            }
+        }
+        exit(EXIT_SUCCESS);
+    } else if (pid > 0) {
+        int status;
+        waitpid(pid, &status, 0);
+    }
+}
+
+static void wtfkde()
+{
+    // Thanks to the authors of the many SO answers I looked at just to get this
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        if (setuid(orig_user) != -1) {
+            int fd = open("/dev/null", O_WRONLY);
+            dup2(fd, STDOUT_FILENO);
+            dup2(fd, STDERR_FILENO);
+            close(fd);
+
+            execl("/usr/bin/kcminit", "/usr/bin/kcminit", "kcm_touchpad", NULL);
+        }
+        exit(EXIT_FAILURE);
+    } else if (pid > 0) {
+        int status;
+        waitpid(pid, &status, 0);
+    }
+}
+
+static void on_screensaver(GDBusProxy *proxy G_GNUC_UNUSED, gchar *sender_name G_GNUC_UNUSED, gchar *signal_name, GVariant *parameters, gpointer user_data G_GNUC_UNUSED)
+{
+    if (!g_strcmp0(signal_name, "ActiveChanged")) {
+        gboolean locked;
+        g_variant_get(parameters, "(b)", &locked);
+
+        if (modify_x11_layout_options)
+            mess_with_x11s_layout(!locked);
+        if (modify_sysrq)
+            write_sysrq(locked ? "0" : orig_sysrq);
+        lockVT(locked);
+        if (!locked)
+            wtfkde();
+    }
+}
+
+int main()
+{
+    // Drop privs to connect to user's session bus: thanks, https://stackoverflow.com/a/6732456
+    orig_user = getuid();
+    if (seteuid(orig_user) == -1) {
+        perror("failed to drop privs");
+        return EXIT_FAILURE;
+    }
+
+    if (!read_sysrq())
+        return EXIT_FAILURE;
+    modify_sysrq = orig_sysrq[0] != '0';
+
+    modify_x11_layout_options = must_we_mess_with_x11s_layout(&extra_x11_layout_options);
+
+    if (!(screensaver_proxy = g_dbus_proxy_new_for_bus_sync(G_BUS_TYPE_SESSION, G_DBUS_PROXY_FLAGS_DO_NOT_LOAD_PROPERTIES, NULL, "org.freedesktop.ScreenSaver", "/org/freedesktop/ScreenSaver", "org.freedesktop.ScreenSaver", NULL, NULL))) {
+        g_printerr("Failed to connect to Screensaver interface on user's session\n");
+        return EXIT_FAILURE;
+    }
+    g_signal_connect(screensaver_proxy, "g-signal", G_CALLBACK(on_screensaver), NULL);
+
+    loop = g_main_loop_new(NULL, FALSE);
+    g_unix_signal_add(SIGINT, on_sigint, NULL);
+    g_unix_signal_add(SIGTERM, on_sigint, NULL);
+
+    if (seteuid(0) == -1) {
+        perror("Failed to regain root privs");
+        return EXIT_FAILURE;
+    }
+    setuid(0);
+
+    g_main_loop_run(loop);
+
+    cleanup();
+    return EXIT_SUCCESS;
+}
